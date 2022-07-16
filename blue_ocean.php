8bc437e77d9db8d61f6b5e3f470f9c7a2c4f3c4b
adman core <admancore@gmail.com>
2022-07-16 12:17:31 +0800

    finish 2

--- blue_ocean.php
+++ blue_ocean.php
@@ -1,17 +1,4 @@
 <?php 
-// function blueOcean($a,$b){
-
-//     $bCopy = $b;
-// $c = array_filter($a, function($item) use(&$bCopy) {
-//     $idx = array_search($item, $bCopy);
-//     // remove it from $b if found
-//     if($idx !== false) unset($bCopy[$idx]);
-//     // keep the item if not found
-//     return $idx === false;
-// });
-// return $c;
-// };
-// echo var_dump(blueOcean([1,5,5,5,5,3], [5]));
 
 function blueOcean($a,$b){
     $array1 = $a;
